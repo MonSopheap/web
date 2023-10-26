6919660ab55dcd6fc020d02f12b71f962fc0e7bc
Mon Sopheap <mon.sopheap27@gmail.com>
2023-10-26 15:09:12 +0700

    update

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,7 +3,8 @@ import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
 import twitter from "./twitter.svg";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import logo from "./logo.png";
 
 export {
   google,
